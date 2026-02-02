d4a16224dd3ec2faa43725b162faf3eac42007ab
JoaoPLS-1 <barrancoij@gmail.com>
2026-02-02 20:18:25 -0300

    React - UseState count

--- todo-list/src/App.tsx
+++ todo-list/src/App.tsx
@@ -1,35 +1,28 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+
+function App() {
+  const [count, setCount] = useState<number>(0)
+  const [resultado, setResultado] = useState("par")
+
+  //Criar um novo estado, para verificar se o valor de cont é par ou impar
+  //imprimir PAR ou IMPAR na tela!
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button onClick={() => {
+        setCount((oldstate) => oldstate + 1)
+        const countAtual = count + 1
+        setResultado(countAtual % 2 === 0 ? "par" : "impar")
+      }}>
+        count is {count}
+
+      </button>
+      <p>{resultado}</p>
     </>
   )
 }
 
 export default App
+
